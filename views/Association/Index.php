eadd53f5d8a31931503274051d6fd6a06217afd7
thibaut88 <code.thibaut@gmail.com>
2016-11-05 18:13:45 +0100

    pagae partenaire layout fini

--- views/Association/Index.php
+++ views/Association/Index.php
@@ -107,7 +107,7 @@
                     <div class="row margin-vert-30">
                         <!-- Main Text -->
                         <div class="col-md-6">
-                            <h2>Welcome to Habitat</h2>
+                            <h2>Bienvenue sur le site aldn</h2>
                             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
                                 lobortis nisl ut aliquip ex ea commodo consequat.</p>
                             <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit
--- views/Partenaires/Index.php
+++ views/Partenaires/Index.php
@@ -1,145 +1,73 @@
-
-	<div class=" container"style="">
-		<div id="partenaires"class="row-padding">
-		
-		<?php
-		$datas = $this->datas;
-
-		foreach ($datas as $key=>$value){ ?>
-		<div class="col-lg-2" >
-		<p class=""><?=ucfirst($value['nom'])?></p>
-		<img src="<?=$value['image']?>" width="50%"height="80%"title="image"alt="image">
-		<p class=" "style="overflow:hidden"><?=$value['description']?></p>
-		</div>
-
-		<?php
-		};
-		?>
-
-		</div><!--#id partenaires row -->
-	</div><!--container -->
 <div class="container">
 
+		<div id="partenaires"class="row-padding">
 <!-- Portfolio -->
             <div id="portfolio" class="bottom-border-shadow">
                 <div class="container bottom-border">
                     <div class="row padding-top-40">
                         <ul class="portfolio-group">
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeInLeft">
-                                        <img alt="image1" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image1.jpg">
+
+		<?php
+		$datas = $this->datas;
+		$i=0;
+		$hr=3;
+		foreach ($datas as $key=>$value){ $i++;
+		?>
+
+		
+		                   <!-- Portfolio Item -->
+                            <li class="portfolio-item col-xs-12 col-sm-3 col-xs-4 margin-bottom-40"style="min-width:200px!important;">
+                                <a href="#" style="display:block;width:80%;margin:auto;">
+                                    <figure class="animate fadeInLeft" >
+                                        <img src="<?=$value['image']?>" width="100%"height="160px;"title="image"alt="image" >
                                         <figcaption>
-                                            <h3>Velit esse molestie</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
+                                            <h3><?=ucfirst($value['nom'])?></h3>
+                                            <span style="overflow:hidden"><?=$value['description']?></span>
                                         </figcaption>
                                     </figure>
                                 </a>
                             </li>
-                            <!-- //Portfolio Item// -->
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeIn">
-                                        <img alt="image2" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image2.jpg">
-                                        <figcaption>
-                                            <h3>Quam nunc putamus</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </li>
-                            <!-- //Portfolio Item// -->
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeInRight">
-                                        <img alt="image3" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image3.jpg">
-                                        <figcaption>
-                                            <h3>Placerat facer possim</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </li>
-                            <!-- //Portfolio Item// -->
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeInLeft">
-                                        <img alt="image4" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image4.jpg">
-                                        <figcaption>
-                                            <h3>Nam liber tempor</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </li>
-                            <!-- //Portfolio Item// -->
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeIn">
-                                        <img alt="image5" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image5.jpg">
-                                        <figcaption>
-                                            <h3>Donec non urna</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </li>
-                            <!-- //Portfolio Item// -->
-                            <!-- Portfolio Item -->
-                            <li class="portfolio-item col-sm-4 col-xs-6 margin-bottom-40">
-                                <a href="#">
-                                    <figure class="animate fadeInRight">
-                                        <img alt="image6" src="/www/PROJETS EXTERNES/ALDNTemplate4/ALDN4/site/assets/img/frontpage/image6.jpg">
-                                        <figcaption>
-                                            <h3>Nullam consectetur</h3>
-                                            <span>Vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui et everti tamquam suavitate mea.</span>
-                                        </figcaption>
-                                    </figure>
-                                </a>
-                            </li>
-                            <!-- //Portfolio Item// -->
+		
+		<?php
+		}
+		?>
+
                         </ul>
                     </div>
                 </div>
             </div>
             <!-- End Portfolio -->
-
+		</div><!--#id partenaires row -->
 
 </div>
 
-		<script>
-		$(document).ready(function(){
+
+
+
+		<script type="text/javascript">
+		
+		
+		// $(document).ready(function(){
 			
-			var $parts = $('.Partenaire');
-			var $partsLen = $parts.length;
-			$parts.each(function(){
-				$(this).css('display','none');
-			});
+			// var $parts = $('.Partenaire');
+			// var $partsLen = $parts.length;
+			// $parts.each(function(){
+				// $(this).css('display','none');
+			// });
 
-		var varCounter = 0;
-		var showPartenaire = function(){
-			 if(varCounter <= $partsLen) {
-				 var $elem = $($parts[varCounter]);
-				$elem.fadeIn('slow');
-				  varCounter++;
-			 } else {
-				  clearInterval(showPartenaire);
-			 }
-		};
+		// var varCounter = 0;
+		// var showPartenaire = function(){
+			 // if(varCounter <= $partsLen) {
+				 // var $elem = $($parts[varCounter]);
+				// $elem.fadeIn('slow');
+				  // varCounter++;
+			 // } else {
+				  // clearInterval(showPartenaire);
+			 // }
+		// };
 
-						 setInterval(showPartenaire, 230);
+						 // setInterval(showPartenaire, 230);
 
-			
-
-
-
-
-			
-		});
+		// });
 
 		</script>
